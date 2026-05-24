753bdac510f54e02fd51a999b616cb19fd485d4c
karamkaur863 <karamkaur863@gmail.com>
2026-05-24 08:57:26 +0530

    Create placeholder for Accounting module

---
+++ Modules/Accounting/AccountingModule.cs
@@ -0,0 +1,8 @@
+// This file is a placeholder for advanced Accounting logic.
+namespace DKGST.Modules.Accounting
+{
+    public static class AccountingModule
+    {
+        // Future: Add services/endpoints for accounting flows.
+    }
+}
